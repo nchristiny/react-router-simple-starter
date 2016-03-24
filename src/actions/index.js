c265e85509800774c97c5375b7ee24a5fdf44caa
Nick Christiny <nchristiny@jellyvision.com>
2016-03-24 11:54:55 -0500

    Implement createPost request using redux-form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=sdgkj93nnljnv0d4977hergrgfa';
+
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
@@ -12,3 +14,13 @@ export function fetchPosts() {
     payload: request
   };
 }
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: {title, categories, content }, handleSubmit } = this.props;
+    // the above is the ES6 equivalant of:
+    // const title = this.props.fields.title
+
+    // below we see the spread operator "..."
+    // it "destructures" the object. Passes on the object input without having
+    // to use this.props.formProps, it just separates the keys and values that
+    // we see in console for title, like onChange, is seen like:
+    // onChange={title.onChange}
     return (
-      <div>
-        Create Form!
-      </div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a new post!</h3>
+        <div className="form-group">
+          <label>Title:</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label>Categories:</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group">
+          <label>Content:</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit!</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps.
+// reduxForm: 1st is form config, 2nd is mapStateToProps, mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
+
+// The above goes to formReducer which updates app state.
+// For example: User types something in and record it in application state.
+// state === {
+//   form: {
+//     PostsNewForm: {
+//       title: {...},
+//       categories: {...},
+//       content: {...}
+//     }
+//   }
+// }
